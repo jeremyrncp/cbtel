26ebc772a6152508d5c1d370da7f79854a844791
Jérémy <jeremy@MacBook-Pro-de-Jeremy.local>
2024-12-04 09:50:51 +0100

    fix

--- src/Form/ResetPasswordType.php
+++ src/Form/ResetPasswordType.php
@@ -6,12 +6,34 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class ResetPasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('password', PasswordType::class, ['label' => 'Mot de passe'])
+        $builder->add('password', PasswordType::class,
+            [
+                'label' => 'Mot de passe',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'max' => 255,
+                    ]),
+                    new Regex([
+                        'pattern' => '/\d+/i',
+                    ]),
+                    new Regex([
+                        'pattern' => '/[,#?!@$%^&*-]+/i',
+                    ]),
+                ],
+            ])
                 ->add('submit', SubmitType::class, ['label' => 'Sauvegarder'])
         ;
     }
